2f0ae09a3a5283ef22385c77c2f8f1b6a8bbec21
zJustHow <zjusthow@gmail.com>
2025-10-01 08:33:54 -0700

    Create TagRepository.java

---
+++ backend/src/main/java/com/laioffer/flagcamp/backend/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.laioffer.flagcamp.backend.repository;
+
+import com.laioffer.flagcamp.backend.entity.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+}
