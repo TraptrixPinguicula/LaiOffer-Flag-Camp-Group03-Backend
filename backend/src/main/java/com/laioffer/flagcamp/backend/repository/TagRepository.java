095fd88c07fc0c44b5fb27994b3a03800ccc21ca
zJustHow <zjusthow@gmail.com>
2025-10-01 12:32:36 -0700

    Update TagRepository.java

--- backend/src/main/java/com/laioffer/flagcamp/backend/repository/TagRepository.java
+++ backend/src/main/java/com/laioffer/flagcamp/backend/repository/TagRepository.java
@@ -1,12 +1,12 @@
 package com.laioffer.flagcamp.backend.repository;
 
 import com.laioffer.flagcamp.backend.entity.Tag;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TagRepository extends CrudRepository<Tag, Long> {
+public interface TagRepository extends ListCrudRepository<Tag, Long> {
     Optional<Tag> findByTagContent(String tagContent);
 }
