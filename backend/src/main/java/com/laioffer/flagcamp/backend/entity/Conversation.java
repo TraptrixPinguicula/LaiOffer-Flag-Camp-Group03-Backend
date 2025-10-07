a33544c66c4c9a6e5de395af66dca53ffcad43e0
zJustHow <zjusthow@gmail.com>
2025-10-07 11:59:35 -0700

    Update Conversation.java

--- backend/src/main/java/com/laioffer/flagcamp/backend/entity/Conversation.java
+++ backend/src/main/java/com/laioffer/flagcamp/backend/entity/Conversation.java
@@ -3,6 +3,7 @@ package com.laioffer.flagcamp.backend.entity;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import java.time.LocalDateTime;
 
 @Table("conversations")
 public record Conversation(
