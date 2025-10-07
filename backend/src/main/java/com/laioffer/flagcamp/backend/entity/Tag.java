88f1fd15504b2e5d8184cc5e9ec6f3dbdb25f276
zJustHow <zjusthow@gmail.com>
2025-10-07 11:18:32 -0700

    Update Tag.java

--- backend/src/main/java/com/laioffer/flagcamp/backend/entity/Tag.java
+++ backend/src/main/java/com/laioffer/flagcamp/backend/entity/Tag.java
@@ -2,7 +2,6 @@ package com.laioffer.flagcamp.backend.entity;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column; // 1. Add this import
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("tags")
