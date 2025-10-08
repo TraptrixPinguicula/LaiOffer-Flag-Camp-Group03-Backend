68e49381c4ca369876a6fc58028a539af43171cb
TraptrixPinguicula <weipengy233@gmail.com>
2025-10-08 14:08:47 +0800

    Fix typo

--- backend/src/main/java/com/laioffer/flagcamp/backend/entity/Message.java
+++ backend/src/main/java/com/laioffer/flagcamp/backend/entity/Message.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Table("messages")
 public record Message(
         @Id Long messageId,
-        @NotNull Long sellerId,
+        @NotNull Long senderId,
         @NotNull Long conversationId,
         @NotNull String messageContent,
         @NotNull LocalDateTime createdAt
